6ed8911c50ae6f2b20a7498062930ab65fb52651
mocbaltig <lekjonde@gmail.com>
2026-05-07 11:57:10 +0700

    feat(vite): use env from `server/.env`

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '../server/.env' });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+
+const PORT = process.env['PORT']
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:3000', changeOrigin: true },
-      '/health': { target: 'http://localhost:3000', changeOrigin: true },
-      '/metrics': { target: 'http://localhost:3000', changeOrigin: true },
+      '/api': { target: `http://localhost:${PORT}`, changeOrigin: true },
+      '/health': { target: `http://localhost:${PORT}`, changeOrigin: true },
+      '/metrics': { target: `http://localhost:${PORT}`, changeOrigin: true },
     },
   },
 });
