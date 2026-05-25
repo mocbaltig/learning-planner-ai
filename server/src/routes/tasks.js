b9b5b2ca9dfa3e910e642d5705ee5a0e46eaf780
mocbaltig <lekjonde@gmail.com>
2026-05-25 14:18:08 +0700

    feat(server): patch task route

--- server/src/routes/tasks.js
+++ server/src/routes/tasks.js
@@ -6,12 +6,13 @@ const router = express.Router();
 
 const authenticate = require('../middleware/authenticate');
 const { validate } = require('../middleware/validate');
-const { taskPayloadSchema } = require('../validator/task-schema');
-const { createTask, getTasksByWeekStart, editStatus } = require('../controller/tasks');
+const { taskPayloadSchema, taskUpdatePayloadSchema } = require('../validator/task-schema');
+const { createTask, getTasksByWeekStart, editStatus, editTask } = require('../controller/tasks');
 
 router.post('/', authenticate, validate(taskPayloadSchema), createTask);
 router.get('/', authenticate, getTasksByWeekStart);
 router.patch('/:id/status', authenticate, editStatus);
+router.patch('/:id', authenticate, validate(taskUpdatePayloadSchema), editTask);
 
 // router.get('/', authenticate, async (req, res, next) => {
 //   try {
