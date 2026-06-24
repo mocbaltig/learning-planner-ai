cfa505423dd2e4e401c981b8b8c09d13cda52686
dimasas <dimasahda@yahoo.com>
2026-06-24 16:52:13 +0700

    update icon

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -42,11 +42,7 @@ export default function Register() {
   return (
     <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
       <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-        <img
-          alt='AI Learning Plan'
-          src='https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500'
-          className='mx-auto h-10 w-auto'
-        />
+        <img src="/images/task-suggestion-logo.png" alt="AI Learning Plan" className='mx-auto h-35 w-auto'/>
         <h2 className='mt-10 text-center text-2xl/9 font-bold tracking-tight text-white'>
           Buat akun baru
         </h2>
