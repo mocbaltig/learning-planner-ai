626c6d9454723c72cba05a0871d2354abece8879
M.Hilman Riski <mhilmanriski@gmail.com>
2026-06-25 10:29:21 +0700

    fix vite local

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // Gunakan nama service Docker, bukan localhost
-        target: 'http://server:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
