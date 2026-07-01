d323577eeb2f02291e0a544497706d9e05f917fe
mocbaltig <lekjonde@gmail.com>
2026-07-02 00:13:58 +0700

    fix(vite.config.js): proxy for `/health` & `/metrics`

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,14 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   server: {
     host: '0.0.0.0',
     proxy: {
-      '/api': {
-        // Gunakan nama service Docker, bukan localhost
-        target: 'http://server:3000',
-        changeOrigin: true,
-      },
+      '/api': { target: 'http://server:3000', changeOrigin: true },
+      '/health': { target: 'http://server:3000', changeOrigin: true },
+      '/metrics': { target: 'http://server:3000', changeOrigin: true },
     },
   },
-});
+});
